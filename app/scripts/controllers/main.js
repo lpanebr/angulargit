811d224f03f74d70941c92ff0fe70be9012fe3f1
Luciano Panepucci <lpanebr@gmail.com>
2014-04-10 09:07:59 -0300

    yo angular baby without Bootstrap

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('provados9App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
