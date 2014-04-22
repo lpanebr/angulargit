91edf11525461a224b103dd48cb689b6362f2499
aedram <rafael.magno@gmail.com>
2014-04-22 09:14:00 -0300

    adding new controllers, views and routes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,6 +13,14 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angulargitApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
